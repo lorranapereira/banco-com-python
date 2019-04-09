0e0059e8577a3aaceddcd81a853e55e2d042a81a
lorranapereira <lorrana_pereira123@hotmail.com>
2019-04-09 18:07:01 -0300

    Add files via upload

---
+++ bancopython.py
@@ -0,0 +1,31 @@
+import psycopg2
+
+con = psycopg2.connect(host='localhost', database='tiago', user='postgres', password='postgres')
+cur = con.cursor()
+#listar limit ofsset se tiver se tiver com id alterar, se não tiver criar 
+#sql = "select *from alunos"
+#cur.execute(sql)
+#for linha in cur.fetchall():
+    #print(linha)
+
+#con.close() 
+#inserir
+#cur.execute("insert into alunos (aluno, id) values (%s,%s)",["guilherme gordao",3])
+#con.commit()
+
+#alterar
+#cur.execute("update alunos set aluno = %s  where id = (%s)",["lorrana_lindona",3])
+#con.commit()
+
+#deletar 
+#cur.execute("delete from alunos where id = (%s)",[3])
+#con.commit()
+#buscar 
+
+#buscar
+cur.execute("select *from alunos where id = (%s)",[4])
+con.commit()
+print(cur.fetchone())
+
+
+con.close() 
